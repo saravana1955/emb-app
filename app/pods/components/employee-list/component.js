fc676dff9d581be162a22d2941a15f04a48e7693
AnishVictor-provility <anish@provility.com>
2021-07-20 18:34:21 +0530

    Updated Header

---
+++ app/pods/components/employee-list/component.js
@@ -0,0 +1,29 @@
+import Component from '@ember/component';
+import {inject as service} from '@ember/service';
+
+
+export default Component.extend({
+  store: service(),
+    
+  init() {
+    this._super(...arguments);
+    const employees = [
+        {
+          name: 'john',
+          age: 20,
+          salary: 10
+        },
+        {
+          name: 'george',
+          age: 25,
+          salary: 20
+        },
+        {
+          name: 'kumar',
+          age: 30,
+          salary: 35
+        }
+      ];
+      this.set('employees',employees)
+  }
+});
