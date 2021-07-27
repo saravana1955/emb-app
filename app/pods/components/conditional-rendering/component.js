3b2b531b1353daa9ba6e1a7a5aaa66fd1dd5139a
ANISHVICTOR <anish@provility.com>
2021-07-27 16:53:01 +0530

    Updated Header

--- app/pods/components/conditional-rendering/component.js
+++ app/pods/components/conditional-rendering/component.js
@@ -1,56 +1,60 @@
 import Component from '@ember/component';
-
-
-
 export default Component.extend({
-employees:null,
- init() {
-
+  name: 'Anish',
+  employees: null,
+  keys: null,
+  bool:false,
+  employeesList: null,
+  init() {
     this._super(...arguments);
     const employees = [
-        {
-          name: 'john',
-          age: 20,
-          salary: 10,
-          status: "active"
-        },
-        {
-          name: 'george',
-          age: 25,
-          salary: 20,
-          status: "active"
-        },
-        {
-          name: 'kumar',
-          age: 30,
-          salary: 35,
-          status: "inactive"
-        },
-        {
-          name: 'abc',
-          age: 18,
-          salary: 55,
-          status: "active"
-        },
-        {
-          name: 'pqr',
-          age: 22,
-          salary: 75,
-          status: "inactive"
-        }
-      ];
-      this.set('employees',employees)
+      {
+        name: 'john',
+        age: 20,
+        salary: 10,
+        status: 'active',
+      },
+      {
+        name: 'george',
+        age: 25,
+        salary: 20,
+        status: 'inactive',
+      },
+      {
+        name: 'kumar',
+        age: 30,
+        salary: 35,
+        status: 'active',
+      },
+      {
+        name: 'xyz',
+        age: 30,
+        salary: 35,
+        status: 'active',
+      },
+      {
+        name: 'pqr',
+        age: 20,
+        salary: 45,
+        status: 'inactive',
+      },
+    ];
+    const keys = Object.keys(employees[0]);
+    this.set('employees', employees);
+    this.set('employeesList', employees);
+    this.set('keys', keys);
   },
-
-  // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
-  action:{
-    ischecked()
-    {
-      let isChecked = document.getElementById('activepeople');
-      console.log(isChecked);
-      document.querySelector('#activepeople:checked') !== null
+  actions:{
+    isChecked(value){
+      if(value){
+        var empOutput = this.get('employees');
+        this.set('employeesList', empOutput);
+        this.set('bool',false);
+      }else{
+        var activeEmpList = this.get('employeesList').filter((emp) => emp.status == "active")
+        this.set('employeesList', activeEmpList)
+        this.set('bool',true);
+      }
     }
   }
-
 });
-
